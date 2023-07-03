42c5484164440ab8de2224fe8b79341302b06c52
Gabriel Albano <albanogabriel33@gmail.com>
2023-07-03 15:59:03 -0300

    finalizando

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -6,23 +6,23 @@ const Sobre = () => (
   <aside>
     <Titulo fontSize={14}>Sobre</Titulo>
     <Paragrafo tipo="secundario">
-      Atualmente busco ingressar na área de Desenvolvimento Web, unindo os
-      conhecimentos do Desenvolvimento Front-End, UX / UI Design. áreas o qual
-      reconheço ser promissoras e alinhadas com os meus talentos. Através do
-      curso da EBAC de Full Stack Design, estou construindo um projeto que passa
-      por todas as etapas do UX e UI Design, através do modelo de processo de
-      Design, o Double Diamond. O modelo de processo engloba desde a primeira
-      parte da empatia até o Design Thinking, Figma, Wireframe e Protótipos, o
-      UX, Usabilidade, UI, guia WCAG, Heurísticas de Nielsen, Pesquisas e testes
-      com usuário, Fundamento da Gestalt, Mobile First. Na parte de
-      Desenvolvimento Front-end foquei nas hard skills para o desenvolvimento
-      web, como: VSCode, HTML, CSS, JavaScript, SaSS, Bootstrap, Versionamento
-      com o Github, Gulp, Less, Grunt, EMKT, Ajax, Fetch API, Promises, Async
-      Await, Babel, ES6+, TypeScript, para assim focar no Framework/Library
-      ReactJS, atualmente o mais utilizado no mercado. Também fiz o curso da
-      Punk Métrics, olhando para a parte analítica do produto para entender o
-      comportamento dos usuários e estratégias de mensuração alinhas com o
-      negócio
+      Atualmente busco ingressar na área de Desenvolvimento Web, unindo
+      conhecimentos do Desenvolvimento Web e Front-End, UX / UI Design. áreas o
+      qual reconheço serem promissoras e alinhadas com os meus talentos. Através
+      do curso da EBAC de Full Stack Design, estou construindo um projeto que
+      passa por todas as etapas do UX e UI Design e Posteriormente ao
+      desenvolvimento Front-End, através do modelo de processo de Design, o
+      Double Diamond. O modelo de processo engloba desde a primeira parte da
+      empatia até o Design Thinking, Figma, Wireframe e Protótipos, o UX,
+      Usabilidade, UI, guia WCAG, Heurísticas de Nielsen, Pesquisas e testes com
+      usuário, Fundamento da Gestalt, Mobile First. Em Desenvolvimento Front-end
+      foquei nas hard skills para o desenvolvimento web como: VSCode, HTML, CSS,
+      JavaScript, SaSS, Bootstrap, Versionamento com o Github, Gulp, Less,
+      Grunt, EMKT, Ajax, Fetch API, Promises, Async Await, Babel, ES6+,
+      TypeScript, para assim focar no Framework/Library ReactJS, atualmente o
+      mais utilizado no mercado. Também fiz o curso da Punk Métrics, olhando
+      para a parte analítica do produto para entender o comportamento dos
+      usuários e estratégias de mensuração alinhas com o negócio
     </Paragrafo>
     <GitHubStats>
       <img src="https://github-readme-stats.vercel.app/api?username=albanogabriel&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
